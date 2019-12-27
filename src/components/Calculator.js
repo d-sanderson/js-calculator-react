ffb769914664a398102015a523c081779b669253
dsanderson90 <dsanderson3@cnm.edu>
2019-12-27 08:01:03 -0700

    first commit.

---
+++ src/components/Calculator.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+
+
+class Calculator extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      values : [
+
+      ]
+     }
+  }
+  render() {
+    return (
+    <div>
+      I am a fucking calculator :)
+    </div>
+    );
+  }
+}
+
+export default Calculator;
